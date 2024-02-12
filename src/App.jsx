499dabb2219a592cd8c3f225bd85822324056cb7
Сибгатуллина Залия Анисовна <242017@mck-ktits.ru>
2024-02-12 18:54:31 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header  from './components/Header/Header'
+import Banner  from './components/Banner/Banner'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import './Header.css'
+import Logo from '/public/Header/logo.svg'
+import SearchIcon from '/public/Header/search.svg'
+import BasketIcon from '/public/Header/basket.svg'
+import FavoriteIcon from '/public/Header/favorite.svg'
+
+
+export default function Header(){
+    return(
+        <header>
+            <div className="container">
+                <div className="header-content">
+                    <div className="header-logo-nav">
+                        <div className="header__logo">
+                            <a href="" className="header__logo-link">
+                                <img src={Logo} alt="" className="logo" />
+                            </a>
+                        </div>
+
+                        <nav className="header__nav">
+                            <ul className="header__nav-menu">
+                                <li className="header__nav-item ">
+                                    <a href="" className="header__nav-link">ГЛАВНАЯ</a>
+                                </li>
+                                <li className="header__nav-item ">
+                                    <a href="" className="header__nav-link">КАТАЛОГ</a>
+                                </li>
+                                <li className="header__nav-item">
+                                    <a href="" className="header__nav-link">ИНФОРМАЦИЯ</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div className="header__buttons">
+                        <a href="" className="header__buttons-link">
+                            <img src={SearchIcon} alt="" className="header__buttons-img" />
+                        </a>
+                        <a href="" className="header__buttons-link">
+                            <img src={BasketIcon} alt="" className="header__buttons-img" />
+                        </a>
+                        <a href="" className="header__buttons-link">
+                            <img src={FavoriteIcon} alt="" className="header__buttons-img" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
