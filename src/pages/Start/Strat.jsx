b8b6daeb346acec446c3cf8e223b35db45320e99
Сибгатуллина Залия Анисовна <242017@mck-ktits.ru>
2024-02-21 10:27:58 +0300

    com

--- src/pages/Start/Strat.jsx
+++ src/pages/Start/Strat.jsx
@@ -1,9 +1,11 @@
 import Banner from '../../components/Banner/Banner'
+import SliderC from '../../components/Slider/SliderC'
 
 export default function Start () {
     return(
         <>
             <Banner/>
+            <SliderC/>
         </>
     )
 }
